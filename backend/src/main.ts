f9e4fcecb08084724fb858a9a42a087a509c4f84
ninom5 <nmilas00@fesb.hr>
2025-04-29 15:14:58 +0200

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,7 +22,11 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://63.177.51.13/:3000',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
