c9e411164ad36cdf1c6d604035b9b3cbfc62a8c9
ninom5 <nmilas00@fesb.hr>
2025-04-29 14:43:25 +0200

    connect frontend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,14 @@ import { QuestionModule } from './question/question.module';
 import { QuizResultModule } from './quizResult/quizResult.module';
 import { QuizModule } from './quiz/quiz.module';
 import { CategoryModule } from './category/category.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'frontend', 'dist'),
+    }),
     UserModule,
     QuestionModule,
     QuizResultModule,
